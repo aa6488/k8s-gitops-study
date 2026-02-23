6d56d973d0970273f3c5408b8193fd57edad7c5a
aa6488 <aa6488@naver.com>
2026-02-23 23:27:50 +0900

    gitops ã테스트용 ã커밋

---
+++ src/main/java/com/example/demo/RootController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+
+
+
+    @GetMapping("/")
+    public String root() {
+        return "수정전 텍스트 문자열 입니다.";
+    }
+
+}
