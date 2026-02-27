a3bc5090304655fccfc70f4ff153432e37751e2f
aa6488 <aa6488@naver.com>
2026-02-28 00:26:08 +0900

    github actions 동작 및 docker hub 푸시테스트,

--- src/main/java/com/example/demo/RootController.java
+++ src/main/java/com/example/demo/RootController.java
@@ -12,7 +12,7 @@ public class RootController {
     @GetMapping("/")
     public String root() {
 //        return "수정전 텍스트 문자열 입니다.";
-        return "github actions 동작 및 docker hub 푸시테스트";
+        return "github actions 동작 및 docker hub 푸시테스트1";
     }
 
 }
